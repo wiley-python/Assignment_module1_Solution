164b2a973f0a9681379d79edad5a727f4eb70a99
snehawiley <121166792+snehawiley@users.noreply.github.com>
2023-02-27 19:20:02 +0530

    Update test.py

--- test.py
+++ test.py
@@ -2,12 +2,13 @@ import q1
 import q2
 
 
-def test_question1(self):
+def test_question1():
     str = ['Hello Dear', 'Hello Sir', 'take Dear', 'take Sir']
     assert q1.question1() == str
 
 
-def test_question2(self):
-    exp = 2510
-    assert q2.question2(q2.str1) == 2510
+def test_question2():
+    exp = '2510'
+    assert q2.question2(q2.str1) == exp
+
 
